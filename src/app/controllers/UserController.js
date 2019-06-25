90c5695adfb53e8342c694d887a938ff7890bdba
Luiz Batanero <luizbatanero@gmail.com>
2019-06-25 10:21:48 -0300

    User store validation

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,27 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .required()
+        .email(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    try {
+      await schema.validate(req.body, { abortEarly: false });
+    } catch (err) {
+      return res
+        .status(400)
+        .json({ error: 'Validation failed', validationErrors: err.errors });
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
