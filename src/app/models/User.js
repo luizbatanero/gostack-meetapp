c962bcd7c2d0211b10908d473233ce1195ca0921
Luiz Batanero <luizbatanero@gmail.com>
2019-06-27 12:01:09 -0300

    Associate user avatar to file

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -22,6 +22,10 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   async checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
---
+++ src/database/migrations/20190627145500-add-avatar-field-to-users.js
@@ -0,0 +1,18 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'files',
+        key: 'id',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
