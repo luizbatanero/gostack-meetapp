4bb30940f4f4cae36f651c68ed02f1a55eb6a1ce
Luiz Batanero <luizbatanero@gmail.com>
2019-06-27 11:25:42 -0300

    Add file upload route

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize';
 
-import User from '../app/models/User';
 import databaseConfig from '../config/database';
+import User from '../app/models/User';
+import File from '../app/models/File';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
