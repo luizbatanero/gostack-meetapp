f19bc04b36cffc7a22da14216f94abae09a962e2
Luiz Batanero <luizbatanero@gmail.com>
2019-06-28 17:24:12 -0300

    Add past date validation to Meetup creation

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,3 +1,5 @@
+import { parseISO, isBefore } from 'date-fns';
+
 import { storeSchema } from '../validations/Meetup';
 import Meetup from '../models/Meetup';
 
@@ -7,12 +9,16 @@ class MeetupController {
       return res.status(400).json({ error: 'Validation failed' });
     }
 
+    if (isBefore(parseISO(req.body.date), Date.now())) {
+      return res.json({ error: "You can't create a Meetup on a past date" });
+    }
+
     const meetup = await Meetup.create({
       ...req.body,
       user_id: req.userId,
     });
 
-    res.json(meetup);
+    return res.json(meetup);
   }
 }
 
