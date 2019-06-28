4c756faf0e441bd241cc7d3375d4ee81b314f663
Luiz Batanero <luizbatanero@gmail.com>
2019-06-28 11:46:25 -0300

    Fix error typo

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -7,7 +7,7 @@ export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ error: 'Token not provided ' });
+    return res.status(401).json({ error: 'Token not provided' });
   }
 
   const [, token] = authHeader.split(' ');
