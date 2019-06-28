09aef94ef68774d761a112df4334828bcbb8ea94
Luiz Batanero <luizbatanero@gmail.com>
2019-06-28 11:47:53 -0300

    Serve uploaded files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import { resolve } from 'path';
 
 import routes from './routes';
 import './database';
@@ -14,6 +15,10 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
