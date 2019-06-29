1bba26e2c04223d937cea0dc2beba19c6a16ea94
Luiz Batanero <luizbatanero@gmail.com>
2019-06-29 02:58:25 -0300

    Send mail on new subscription

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -1,7 +1,11 @@
 import { Op } from 'sequelize';
+import { format } from 'date-fns';
 
 import Meetup from '../models/Meetup';
 import Subscription from '../models/Subscription';
+import User from '../models/User';
+
+import Mail from '../../lib/Mail';
 
 class SubscriptionController {
   async index(req, res) {
@@ -27,7 +31,10 @@ class SubscriptionController {
   }
 
   async store(req, res) {
-    const meetup = await Meetup.findByPk(req.params.id);
+    const meetup = await Meetup.findByPk(req.params.id, {
+      include: [{ model: User, as: 'user' }],
+    });
+    const user = await User.findByPk(req.userId);
 
     if (!meetup) {
       return res.status(400).json({ error: 'Meetup not found' });
@@ -78,6 +85,19 @@ class SubscriptionController {
     const subscription = await Subscription.create({
       meetup_id: meetup.id,
       user_id: req.userId,
+    });
+
+    await Mail.sendMail({
+      to: `${meetup.user.name} <${meetup.user.email}>`,
+      subject: `Nova inscrição (${meetup.title})`,
+      template: 'new-subscription',
+      context: {
+        organizerName: meetup.user.name,
+        meetupTitle: meetup.title,
+        meetupDate: format(meetup.date, "dd/MM/yyyy 'às' H:mm'h'"),
+        userName: user.name,
+        userEmail: user.email,
+      },
     });
 
     return res.json(subscription);
